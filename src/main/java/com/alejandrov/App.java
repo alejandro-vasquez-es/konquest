0d702ad7ccf4e8aed6cb275b45b9805f11743292
Alejandro <alejandrovasquezesc@gmail.com>
2022-05-10 13:48:03 -0600

    interfaz hecha

--- src/main/java/com/alejandrov/App.java
+++ src/main/java/com/alejandrov/App.java
@@ -1,5 +1,7 @@
 package com.alejandrov;
 
+import com.alejandrov.frontend.Konquest;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Konquest konquest = new Konquest();
+        konquest.setVisible(true);
     }
 }
